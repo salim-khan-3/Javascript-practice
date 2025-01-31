76fcf8996563acc5f265c7e8c4919b60c95cc42f
Md salim islam <salimislam0036@gmail.com>
2025-01-31 21:14:21 +0600

    create different folder

---
+++ Function_Problems/F13.js
@@ -0,0 +1,10 @@
+// function isPassFail(Bangla,english,math,chemistry,biology){
+//     if(Bangla>=33 && english>=33 && math >= 33&& chemistry>=33 && biology>=33){
+//         return "pass"
+//     }else{
+//         return "fail"
+//     }
+// }
+
+// let PassFail = isPassFail(34,35,32.9999,37,33);
+// console.log(PassFail);
--- Function_Problems/F16.js
+++ Function_Problems/F16.js
@@ -3,8 +3,16 @@ function isMorningOrNight(time){
         console.log("Morning");
     }else if(time >= 12 && time < 18){
         console.log("Afternoon");
-    }else{
+    }else if(time > 24){
+        console.log('Invalid time');
+    }
+    else{
         console.log("night");
     }
 }
-isMorningOrNight(10)
+isMorningOrNight(23)
+isMorningOrNight(25)
+isMorningOrNight(7)
+isMorningOrNight(2)
+isMorningOrNight(14)
+
